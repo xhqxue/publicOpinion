abc78b8b1bb0c8ca3563398b6651634d7be49ebd
xhqxue <374280338@qq.com>
2019-12-27 18:55:19 +0800

    50times update

--- main2.r
+++ main2.r
@@ -7,7 +7,7 @@
   p_need<-seq(from=0.2,to=3,by=.2)
   beta_need<-seq(from=0.2,to=1,by=.1)
   num_node<-1000
-  run_time<-20
+  run_time<-50
   li_all<-list()
   g_num<-0
   for(i in seq(p_need)){
@@ -25,4 +25,30 @@
       
     }
   }
+  p_s<-NULL
+  beta_s<-NULL
+  for(i in seq(p_need)){
+    for(ii in seq(beta_need)){
+      p_s<-c(p_s,p_need[i])
+      beta_s<-c(beta_s,beta_need[ii])
+    }
+  }
+  acc_num<-NULL
+  for(i in seq(li_all)){
+    acc_num<-c(acc_num,li_all[[i]][500,"acc_number_all"])
+  }
+  data1<-data.frame(p_s,beta_s,acc_num)
+  
+  data_c<-function(lia,name){
+    aa1<-NULL
+    for(i in seq(li_all)){
+      aa1<-c(aa1,lia[[i]][500,name])
+    }
+    return(aa1)
+  }
+  
+  data3<-data.frame(x1,x2,z)
+  fit3<-cusp(y~z,alpha~x1+x2,beta~x1+x2,data3)
+  
+  
  
